4219a7b73f8ebc40d44a15083d49be6813b2d210
A-Kashif108 <asadullahkashif108@gmail.com>
2022-05-29 22:29:07 +0530

    hosted on github

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { HashRouter,Route, Routes } from "react-router-dom";
 import SignUp from './Pages/SignUp/SignUp';
 import Main from './Pages/Main/main';
 import SignIn from './Pages/SignIn/signIn';
 
 const App1: React.FC=()=> {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/main" element={<Main />} />
         <Route path="/Signup" element={<SignUp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
